e7fa9ed2798031baf879fd37989cc6ad4b2b71ba
minanikolic916 <121025319+minanikolic916@users.noreply.github.com>
2023-05-31 16:05:47 +0200

    Add files via upload
    New version, improved UI

--- mywidget.py
+++ mywidget.py
@@ -1,3 +1,4 @@
+#orange imports
 from Orange.data import Table
 from Orange.base import Learner
 from Orange.base import Model
@@ -5,34 +6,22 @@ from Orange.widgets import gui, settings
 from Orange.widgets.settings import Setting
 from Orange.widgets.gui import QComboBox
 from Orange.widgets.widget import OWWidget, Input, Output, Msg,  AttributeList
-from PyQt5.QtCore import QObject, QThread, pyqtSignal
-import autosklearn.classification
-import sklearn.model_selection
+#scikit imports
 import sklearn.datasets
 import sklearn.metrics
+#utility imports
 import pandas as pd
-import shap
 import numpy as np
-from IPython.display import HTML,display
-from PyQt5.QtCore import QRunnable, Qt, QThreadPool
-from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from sklearn.utils import parallel_backend, register_parallel_backend
-from joblib._parallel_backends import ThreadingBackend
-from sklearn.ensemble import RandomForestClassifier
-from joblib import parallel_backend
-from dask.distributed import Client
-from joblibspark import register_spark
-from lime import lime_tabular
-import os
-from pyqtgraph import PlotWidget, plot
-import pyqtgraph as pg
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-import imgkit
+#pyqt imports
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+#other imports
 from html2image import Html2Image
+from lime import lime_tabular
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import dataframe_image as dfi
 class MyWidget(OWWidget):
     name = "Lime explainable AI"
     description = "Lime for model explainability"
@@ -43,8 +32,7 @@ class MyWidget(OWWidget):
     resizing_enabled = True
     
     label = Setting("")
-
-
+    
     class Inputs:
         data = Input("Data", Table)
         model = Input("Model", Model)
@@ -66,6 +54,8 @@ class MyWidget(OWWidget):
         self.test_instance = None
         self.model = None
 
+        self.layout = QVBoxLayout()
+        self.controlArea.setLayout(self.layout)
         self.instanceInfo = gui.widgetBox(self.controlArea, "Selected instance for explanation")
         self.infoInstanceLoaded = gui.widgetLabel(self.instanceInfo, '')
 
@@ -74,6 +64,7 @@ class MyWidget(OWWidget):
         self.html_radio_button = gui.appendRadioButton(box,"Html", addToLayout=True)
 
         lime_processing_button = gui.button(self.controlArea, self, "Explain prediction", callback = self.graph_choice)
+
 
     @Inputs.data
     def set_data(self, data):
@@ -94,12 +85,19 @@ class MyWidget(OWWidget):
         if instance:
             self.test_instance= instance
             self.instance_info()
+            self.run_lime()
         else:
             self.test_instance = None
-        
 
     def graph_choice(self):
-        self.run_lime()
+        exp = self.run_lime()
+        if(self.plot_radio_button.isChecked()):
+            widg = self.show_plot(exp)
+        else:
+            widg = self.show_html()
+        for i in reversed(range(self.mainArea.layout().count())):
+            self.mainArea.layout().itemAt(i).widget().deleteLater()
+        self.mainArea.layout().addWidget(widg)
                 
     def instance_info(self):
         domain = self.data.domain
@@ -112,11 +110,14 @@ class MyWidget(OWWidget):
         str_feat.append(str(target))
         df = pd.DataFrame(self.test_instance)
         df.columns = str_feat
-        self.infoInstanceLoaded.setText(str(df.iloc[0]))
+        df = df.transpose()
+        dfi.export(df, 'dataframe.png')
+        self.infoInstanceLoaded.setPixmap(QPixmap('dataframe.png'))
+        #self.infoInstanceLoaded.setText(str(df.iloc[0]))
+        #self.infoInstanceLoaded.addWidget(self.image_label_info)
           
 
     def run_lime(self):
-        
         domain = self.data.domain
         target = domain.class_var
         metas = self.data.metas
@@ -141,36 +142,31 @@ class MyWidget(OWWidget):
         #data frame za test instance
         df_instance = pd.DataFrame(self.test_instance)
         df_instance.columns = str_feat
-        df_instance = df[df.columns.difference([str(target)])]
+        df_instance = df_instance[df.columns.difference([str(target)])]
+        print(df_instance)
         df_instance_row = df_instance.iloc[0]
-
+        
         instance_to_explain = df_instance_row
         exp=explainer.explain_instance(instance_to_explain, self.model.predict_proba, num_features=8)
-        self.exp_file= exp.save_to_file('./example/widgets/new.html')
-        if(self.plot_radio_button.isChecked()):
-            widg = self.show_plot(exp)
-        elif(self.html_radio_button.isChecked()):
-            widg = self.show_html()
-        return widg
+        self.exp_file= exp.save_to_file('./example/widgets/results.html')
+        return exp
       
     def show_plot(self,exp):
         self.exp_graph = exp.as_pyplot_figure()
         self.canvas = FigureCanvas(self.exp_graph)
-        self.mainArea.layout().addWidget(self.canvas)
-
+        #self.mainArea.layout().addWidget(self.canvas)
         return self.canvas
 
 
     def show_html(self):
-
-        with open('./example/widgets/new.html', 'r') as f:
+        with open('./example/widgets/results.html', 'r', encoding='utf-8') as f:
             html = f.read()
             hti = Html2Image()
-            hti.screenshot(html_str=html, save_as= 'new.jpg')
+            hti.screenshot(html_str=html, css_str = ['body {background:white;}'], save_as= 'new.jpeg')
             self.image_label = QLabel()
             self.image_label.setAlignment(Qt.AlignCenter)
-            self.image_label.setPixmap(QPixmap('new.jpg').scaled(self.width(), self.height(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
-
+            self.image_label.setPixmap(QPixmap('new.jpeg').scaled(self.width(), self.height(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+            #self.mainArea.layout().addWidget(self.image_label)
         return self.image_label
 
     def commit(self):
@@ -179,6 +175,8 @@ class MyWidget(OWWidget):
     def send_report(self):
         # self.report_plot() includes visualizations in the report
         self.report_caption(self.label)
+            
+    
 
 
 if __name__ == "__main__":
